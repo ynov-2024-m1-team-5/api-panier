5efe4aad19246f2c8794c45233dd41d1fb6050c9
Mougni-Nawel <mougninawel@gmail.com>
2024-03-28 14:28:45 +0100

    token middlewareas

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,7 @@
 const router = require('express').Router();
 const cartRouter = require('./cart.router');
-const verifyToken = require('../middlewares/verifyToken');
 
 
-router.use(verifyToken);
 router.use('/cart', cartRouter);
 
 module.exports = router;
